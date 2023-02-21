b8cd1324ba2f86799ad64fc1aff11c6e1ecbc412
mshaari <michael.shaari@gmail.com>
2023-02-21 17:57:12 -0500

    Starting to add commenting functionality

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -10,4 +11,20 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Project, {
+  foreignKey: 'projet_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Project.hasMany(Comment, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project, Comment };
